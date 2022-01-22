338b4700eacddc6891144de92c5bf405db40ffbb
keisummer <hwangkyungha@naver.com>
2022-01-22 15:49:32 +0900

    프로젝트 생성

---
+++ src/main/java/com/keisummer/webservice/controller/WebRestController.java
@@ -0,0 +1,13 @@
+package com.keisummer.webservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebRestController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "HelloWorld";
+    }
+}
